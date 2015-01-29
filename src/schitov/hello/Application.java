c4d78efbb0fbf3d5c3abeb995bd580d9d07f25f1
alibabaih <alibabaih@gmail.com>
2015-01-29 17:26:45 +0400

    REST application

--- src/schitov/hello/Application.java
+++ src/schitov/hello/Application.java
@@ -19,9 +19,11 @@ public class Application {
         };
     }
 
+    /*
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         MessagePrinter printer = context.getBean(MessagePrinter.class);
         printer.printMessage();
     }
+    */
 }
